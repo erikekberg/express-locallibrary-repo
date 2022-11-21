1cae662267b33dc777740342f5f6891f7384263a
erikekberg <erik.oakmountain@gmail.com>
2022-11-21 16:18:05 +0100

    files added

---
+++ models/author.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
+
+// Virtual for author's full name
+AuthorSchema.virtual("name").get(function () {
+  // To avoid errors in cases where an author does not have either a family name or first name
+  // We want to make sure we handle the exception by returning an empty string for that case
+  let fullname = "";
+  if (this.first_name && this.family_name) {
+    fullname = `${this.family_name}, ${this.first_name}`;
+  }
+  if (!this.first_name || !this.family_name) {
+    fullname = "";
+  }
+  return fullname;
+});
+
+// Virtual for author's URL
+AuthorSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual("lifespan_formatted").get(function () {
+  let birthdate;
+  let deathdate;
+
+  if (this.date_of_birth) {
+    birthdate = DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
+  if (this.date_of_death) {
+    deathdate = DateTime.fromJSDate(this.date_of_death).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
+
+  if (birthdate && deathdate) {
+    return `${birthdate ? birthdate : ""} - ${deathdate ? deathdate : ""}`;
+  } else if (!birthdate) {
+    return "";
+  } else {
+    return `${birthdate}`;
+  }
+});
+
+// Export model
+module.exports = mongoose.model("Author", AuthorSchema);
